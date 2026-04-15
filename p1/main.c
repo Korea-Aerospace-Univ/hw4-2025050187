4dfa1cef214dd044607dc22eed1234a0539f43f1
2025050187 <aslfgy1979@gmail.com>
2026-04-15 15:30:55 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-int main() {
-    int answer, guess, count = 0;
+int main(void)
+{
+    int answer;
+    int guess;
+    int count = 0;
 
-    printf("정답 입력 : ");   /* ← 이 줄 추가 */
+    // 정답 입력
     scanf("%d", &answer);
 
     do {
@@ -11,15 +14,18 @@ int main() {
         count++;
 
         if (guess > answer) {
-            printf("%d>?\n", guess);
-        } else if (guess < answer) {
-            printf("%d<?\n", guess);
-        } else {
-            printf("%d==?\n", guess);
+            printf("%d>? 보다 낮습니다\n", guess);
         }
+        else if (guess < answer) {
+            printf("%d<? 보다 높습니다\n", guess);
+        }
+        else {
+            printf("%d==? 정답입니다\n", guess);
+        }
+
     } while (guess != answer);
 
-    printf("시도횟수는 %d회\n", count);
+    printf("%d 시도횟수는 %d회\n", answer, count);
 
     return 0;
 }
